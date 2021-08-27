cba835cdda27df652b9cb7d8b4e36d39a7d54155
Bruno <ferreira.brunomarcelo@gmail.com>
2021-08-27 03:58:21 -0300

    Start project

---
+++ src/test/java/com/gubeetecnologia/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gubeetecnologia.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
